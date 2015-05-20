96dbfef19a0031074d3f52e88864afee6fb569ab
Mislav Marohnić <mislav.marohnic@gmail.com>
2015-05-20 18:36:35 +0200

    Support `--sh|--no-sh` flags on `commands`

--- commands/commands.go
+++ commands/commands.go
@@ -3,20 +3,27 @@ package commands
 import (
 	"github.com/mislav/everyenv/cli"
 	"sort"
+	"strings"
 )
 
 var commandsHelp = `
-Usage: $ProgramName commands
+Usage: $ProgramName commands [--sh|--no-sh]
 
 List all available $ProgramName commands
 `
 
 func commandsCmd(args cli.Args) {
+	shOnly := args.HasFlag("--sh")
+	noSh := args.HasFlag("--no-sh")
+
 	commandNames := cli.CommandNames()
 	sort.Strings(commandNames)
 
 	for _, name := range commandNames {
-		cli.Println(name)
+		isSh := strings.Index(name, "sh-") == 0
+		if (!shOnly || isSh) && !(noSh && isSh) {
+			cli.Println(name)
+		}
 	}
 }
 
