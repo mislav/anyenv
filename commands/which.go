mislav/anyenv

commit 40f04345823f55fd2e6122f428243dafcd8d1c77

Ensure `ProgramName` identifies itself in stderr messages

commands/which.go
@@ -18,10 +18,14 @@ running the given command.
 func whichCmd(args cli.Args) {
 	currentVersion := detectVersion()
 	exeName := args.Required(0)
-	exePath := findExecutable(exeName, currentVersion)
+	exePath, err := findExecutable(exeName, currentVersion)
+	if err != nil {
+		cli.Errorf("%s: %s\n", args.ProgramName(), err)
+		cli.Exit(1)
+	}
 
 	if exePath.IsBlank() {
-		cli.Errorf("%s: command not found\n", exeName)
+		cli.Errorf("%s: %s: command not found\n", args.ProgramName(), exeName)
 		versions := whence(exeName)
 		if len(versions) > 0 {
 			cli.Errorf("\nThe `%s' command exists in these versions:\n  %s\n", exeName,
@@ -33,25 +37,22 @@ func whichCmd(args cli.Args) {
 	}
 }
 
-func findExecutable(exeName string, currentVersion SelectedVersion) utils.Pathname {
+func findExecutable(exeName string, currentVersion SelectedVersion) (filename utils.Pathname, err error) {
 	if currentVersion.IsSystem() {
-		filename := findInPath(exeName)
-		if !filename.IsBlank() {
-			return filename
-		}
+		filename = findInPath(exeName)
 	} else {
 		versionDir := config.VersionDir(currentVersion.Name)
 		if !versionDir.Exists() {
-			cli.Errorf("version `%s' is not installed\n", currentVersion.Name)
-			cli.Exit(1)
+			err = VersionNotFound{currentVersion.Name}
+			return
 		}
 		filename := versionDir.Join("bin", exeName)
-		if filename.IsExecutable() {
-			return filename
+		if !filename.IsExecutable() {
+			filename = utils.NewPathname("")
 		}
 	}
 
-	return utils.NewPathname("")
+	return
 }
 
 func findInPath(exeName string) utils.Pathname {

commands/exec.go
@@ -18,9 +18,13 @@ Runs an executable by first preparing PATH so that the selected version's
 func execCmd(args cli.Args) {
 	currentVersion := detectVersion()
 	exeName := args.Required(0)
-	exePath := findExecutable(exeName, currentVersion)
-	env := os.Environ()
+	exePath, err := findExecutable(exeName, currentVersion)
+	if err != nil {
+		cli.Errorf("%s: %s\n", args.ProgramName(), err)
+		cli.Exit(1)
+	}
 
+	env := os.Environ()
 	if !currentVersion.IsSystem() {
 		for i, value := range env {
 			if strings.Index(value, "PATH=") == 0 {

commands/global.go
@@ -34,7 +34,8 @@ func globalCmd(args cli.Args) {
 		if assign != "system" {
 			versionDir := config.VersionDir(assign)
 			if !versionDir.Exists() {
-				cli.Errorf("version `%s` not installed\n", assign)
+				err := VersionNotFound{assign}
+				cli.Errorf("%s: %s\n", args.ProgramName(), err)
 				cli.Exit(1)
 			}
 		}

commands/local.go
@@ -44,14 +44,15 @@ func localCmd(args cli.Args) {
 			version, _ := readVersionFile(versionFile)
 			cli.Println(version)
 		} else {
-			cli.Errorln("no local version configured for this directory")
+			cli.Errorf("%s: no local version configured for this directory\n", args.ProgramName())
 			cli.Exit(1)
 		}
 	} else {
 		if assign != "system" {
 			versionDir := config.VersionDir(assign)
 			if !versionDir.Exists() {
-				cli.Errorf("version `%s` not installed\n", assign)
+				err := VersionNotFound{assign}
+				cli.Errorf("%s: %s\n", args.ProgramName(), err)
 				cli.Exit(1)
 			}
 		}

commands/prefix.go
@@ -23,7 +23,8 @@ func prefixCmd(args cli.Args) {
 	if versionDir.Exists() {
 		cli.Println(versionDir)
 	} else {
-		cli.Errorf("version `%s' not installed\n", version)
+		err := VersionNotFound{version}
+		cli.Errorf("%s: %s\n", args.ProgramName(), err)
 		cli.Exit(1)
 	}
 }

commands/rehash.go
@@ -65,7 +65,7 @@ func rehashCmd(args cli.Args) {
 
 	file, err := os.OpenFile(shimFile.String(), os.O_WRONLY|os.O_CREATE|os.O_EXCL, 0755)
 	if err != nil {
-		cli.Errorf("cannot rehash: %s\n", err)
+		cli.Errorf("%s: cannot rehash: %s\n", args.ProgramName(), err)
 		cli.Exit(1)
 	}
 

commands/version.go
@@ -17,6 +17,14 @@ func (ver SelectedVersion) IsSystem() bool {
 	return "system" == ver.Name
 }
 
+type VersionNotFound struct {
+	Version string
+}
+
+func (err VersionNotFound) Error() string {
+	return "version `" + err.Version + "' is not installed"
+}
+
 var versionHelp = `
 Usage: $ProgramName version
 

commit 8db08dbd9cd25c7962748652f6f44c5b91c6e29b

Fix `findExecutable`
The `filename` variable shouldn't get redefined.

This was broken in 40f04345823f55fd2e6122f428243dafcd8d1c77

commands/which.go
@@ -46,7 +46,7 @@ func findExecutable(exeName string, currentVersion SelectedVersion) (filename ut
 			err = VersionNotFound{currentVersion.Name}
 			return
 		}
-		filename := versionDir.Join("bin", exeName)
+		filename = versionDir.Join("bin", exeName)
 		if !filename.IsExecutable() {
 			filename = utils.NewPathname("")
 		}