ab905d6d8b246681da7a62fa3516b880d4773d0c
Mislav Marohnić <mislav.marohnic@gmail.com>
2015-12-25 20:53:12 +0100

    Fully compatible `exec` hook
    - Provides values within hook:
      - RBENV_VERSION
      - RBENV_COMMAND
      - RBENV_COMMAND_PATH
      - RBENV_BIN_PATH
    - Main process adopts changes to these values:
      - RBENV_COMMAND
      - RBENV_COMMAND_PATH
      - RBENV_BIN_PATH
    - Main process adopts changes to positional parameters.

--- commands/exec.go
+++ commands/exec.go
@@ -2,9 +2,9 @@ package commands
 
 import (
 	"github.com/mislav/anyenv/cli"
-	"github.com/mislav/anyenv/config"
 	"github.com/mislav/anyenv/utils"
 	"os/exec"
+	"strconv"
 	"strings"
 	"syscall"
 )
@@ -25,37 +25,79 @@ func execCmd(args cli.Args) {
 		cli.Exit(1)
 	}
 
+	binPath := exePath.Dir()
 	env := utils.EnvFromEnviron()
+	argv := args.ARGV[3:]
 
 	hooks := findHookScripts("exec")
 	if len(hooks) > 0 {
-		hookArgs := append([]string{"-c", `
-			for file; do source "$file"; done
+		hookEnv := env.Clone()
+		hookEnv.Set("RBENV_VERSION", currentVersion.Name)
+		hookEnv.Set("RBENV_COMMAND", exeName)
+		hookEnv.Set("RBENV_COMMAND_PATH", exePath.String())
+		hookEnv.Set("RBENV_BIN_PATH", binPath.String())
+
+		hookArgs := []string{"-e", "-c", `
+			scripts=$1; shift 1
+			while [ $((scripts--)) -gt 0 ]; do source "$1"; shift 1; done
+			echo anyenv -x "RBENV_COMMAND=${RBENV_COMMAND}"
+			echo anyenv -x "RBENV_COMMAND_PATH=${RBENV_COMMAND_PATH}"
+			echo anyenv -x "RBENV_BIN_PATH=${RBENV_BIN_PATH}"
 			export
-		`, "--"}, hooks...)
+			printf "%s\n" "---"
+			for arg; do printf "%s\0" "$arg"; done
+		`, "--", strconv.Itoa(len(hooks))}
+		hookArgs = append(hookArgs, hooks...)
+		hookArgs = append(hookArgs, argv...)
+
 		hookCmd := exec.Command("bash", hookArgs...)
+		hookCmd.Env = hookEnv.Environ()
+		hookCmd.Stderr = cli.Stderr
+
 		hookOut, err := hookCmd.Output()
 		if err != nil {
-			panic(err)
+			cli.Errorf("%s: %s in `exec' hook\n", args.ProgramName(), err)
+			cli.Exit(1)
 		}
+
 		lines := strings.Split(string(hookOut), "\n")
+		positionalLines := []string{}
+		readingPositional := false
+
 		for _, line := range lines {
-			if strings.HasPrefix(line, "declare -x ") {
+			if readingPositional {
+				positionalLines = append(positionalLines, line)
+			} else if strings.HasPrefix(line, "declare -x ") {
 				pair := strings.SplitN(strings.TrimPrefix(line, "declare -x "), "=", 2)
 				if len(pair) > 1 {
 					env.Set(pair[0], strings.Trim(pair[1], "\""))
 				}
+			} else if strings.HasPrefix(line, "anyenv -x ") {
+				pair := strings.SplitN(strings.TrimPrefix(line, "anyenv -x "), "=", 2)
+				if len(pair) > 1 {
+					switch pair[0] {
+					case "RBENV_COMMAND":
+						exeName = pair[1]
+					case "RBENV_COMMAND_PATH":
+						exePath = utils.NewPathname(pair[1])
+					case "RBENV_BIN_PATH":
+						binPath = utils.NewPathname(pair[1])
+					}
+				}
+			} else if line == "---" {
+				readingPositional = true
 			}
 		}
+
+		positionalSerialized := strings.Join(positionalLines, "\n")
+		argv = strings.Split(strings.TrimSuffix(positionalSerialized, "\x00"), "\x00")
 	}
 
 	if !currentVersion.IsSystem() {
-		versionBindir := config.VersionDir(currentVersion.Name).Join("bin")
-		env.Set("PATH", versionBindir.String()+":"+env.Get("PATH"))
+		env.Set("PATH", binPath.String()+":"+env.Get("PATH"))
 	}
 
-	argv := []string{exeName}
-	argv = append(argv, args.ARGV[3:]...)
+	argv = append([]string{exeName}, argv...)
 
 	err = syscall.Exec(exePath.String(), argv, env.Environ())
 	if err != nil {
